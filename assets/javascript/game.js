10657618b099f0145be408900e1fabb2282e602d
Scott <scott.olafsen@gmail.com>
2018-11-16 00:50:02 -0700

    second draft

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,49 @@
 $(document).ready(function () {
     $(".restart").hide();
-    $(".attack").show();
-
+    $(".attack").hide();
+    
+    // Character Attributes
     var charImage = ["https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/12143343_10153502487500020_2287617928083366756_n.jpg?_nc_cat=111&_nc_ht=scontent-sjc3-1.xx&oh=d23cf26834acf34247ee375297cc8a7d&oe=5C82CF06", "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/10489840_10102333562189519_9211457176484726882_n.jpg?_nc_cat=106&_nc_ht=scontent-sjc3-1.xx&oh=d33dbeb25fa18dca2d644025163bb968&oe=5C81B3C9", "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/36889380_10215492856851028_1348281412075126784_n.jpg?_nc_cat=105&_nc_ht=scontent-sjc3-1.xx&oh=f45c9364e6fba9bfd64d17f888cc7959&oe=5C75BEB6", "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/10489720_10154408757045301_4872662977005350940_n.jpg?_nc_cat=103&_nc_ht=scontent-sjc3-1.xx&oh=717b3cdf19e497e2e1a269f82f566c3e&oe=5C6ADF6B"];
     var name = ["River Guide", "Ski Patroller", "Mountain Guide", "Fishing Guide"];
-
     var healthPoints = [120, 100, 150, 180];
     var attack = [8, 10, 6, 4];
     var baseAttack = [8, 10, 6, 4];
     var counterAttack = [10, 5, 20, 25];
+    
+    // Global Game Variables
     var charIndex;
     var defenderPresent = false;
     var defenderIndex;
     var wins = 0;
+    
 
+    // Button to Start Game
+    $(".start").on("click", function () {
+        $(".start").hide();
+        $(".attack").show();
+        startGame(); 
+    });
+   
 
-
-
-    // Character Generation
+    // Generates Characters in Choose Your Character Section
+    var startGame = function() {
     for (var i = 0; i < charImage.length; i++) {
         var player = $("<img>");
         player.attr("src", charImage[i]);
-        player.attr("HP", healthPoints[i]);
-        player.attr("attack", attack[i]);
-        player.attr("counter-attack", counterAttack[i]);
-        player.attr("index", i);
         var headline = $("<p>");
         headline.text(name[i]);
         var health = $("<p>");
         health.addClass("health" + i)
         health.text(healthPoints[i]);
-        $("#" + i).append(health);
-        $("#" + i).append(headline);
-        $("#" + i).append(player);
+        $("#" + i).append(health, headline, player);
+        }
+    }   
+    
 
-    }
-
-
-
+    // Generates clicked character in Your Character section
     $(".choose-character").on("click", function () {
         var charIndex = ($(this).attr("id"));
         charIndex = parseInt(charIndex);
-        console.log(charIndex);
         var generateYourChar = function () {
             for (var i = 0; i < charImage.length; i++) {
                 if (i != charIndex) {
@@ -49,21 +51,16 @@ $(document).ready(function () {
                 }
                 var player = $("<img>");
                 player.attr("src", charImage[i]);
-                player.attr("HP", healthPoints[i]);
-                player.attr("attack", attack[i]);
-                player.attr("counter-attack", counterAttack[i]);
-                player.attr("index", i);
                 var headline = $("<p>");
                 headline.text(name[i]);
                 var health = $("<p>");
                 health.addClass("health" + i)
                 health.text(healthPoints[i]);
-                $("#" + (i + 10)).append(health);
-                $("#" + (i + 10)).append(headline);
-                $("#" + (i + 10)).append(player);
+                $("#" + (i + 10)).append(health, headline, player);
                 attackerIndex = charIndex;
             }
         }
+        // Generates the 3 Enemey characters in Enemy section
         var generateEnemyChar = function () {
             for (var i = 0; i < charImage.length; i++) {
                 if (i === charIndex) {
@@ -71,18 +68,12 @@ $(document).ready(function () {
                 }
                 var player = $("<img>");
                 player.attr("src", charImage[i]);
-                player.attr("HP", healthPoints[i]);
-                player.attr("attack", attack[i]);
-                player.attr("counter-attack", counterAttack[i]);
-                player.attr("index", i);
                 var headline = $("<p>");
                 headline.text(name[i]);
                 var health = $("<p>");
                 health.addClass("health" + i)
                 health.text(healthPoints[i]);
-                $("#" + (i + 20)).append(health);
-                $("#" + (i + 20)).append(headline);
-                $("#" + (i + 20)).append(player);
+                $("#" + (i + 20)).append(health, headline, player);
                 $("#0, #1, #2, #3").empty();
             }
         }
@@ -90,42 +81,39 @@ $(document).ready(function () {
         generateEnemyChar();
     });
 
+
+    // Sends Clicked Enemy into Defender area
     $(".enemy").on("click", function () {
+        if (defenderPresent) {
+            return
+        }
         var defender = ($(this).attr("id"));
         defender = parseInt(defender);
         defender = defender - 20;
-        console.log(defender);
         var pickEnemy = function () {
             for (var i = 0; i < charImage.length; i++) {
-                if (defenderPresent) {
-                    return
-                }
-                else if (i != defender) {
+                 if (i != defender) {
                     continue;
                 }
                 var player = $("<img>");
                 player.attr("src", charImage[i]);
-                player.attr("HP", healthPoints[i]);
-                player.attr("attack", attack[i]);
-                player.attr("counter-attack", counterAttack[i]);
-                player.attr("index", i);
                 var headline = $("<p>");
                 headline.text(name[i]);
                 var health = $("<p>");
                 health.addClass("health" + i)
                 health.text(healthPoints[i]);
-                $("#" + (i + 30)).append(health);
-                $("#" + (i + 30)).append(headline);
-                $("#" + (i + 30)).append(player);
+                $("#" + (i + 30)).append(health, headline, player);
                 $("#" + (defender + 20)).empty();
-                defenderIndex = defender;
                 $("#theAttack, #theCounterAttack").empty();
+                defenderIndex = defender;
             }
         }
         pickEnemy();
         defenderPresent = true;
     });
 
+
+    // Attack Button Main Game Process
     $(".attack").on("click", function () {
         if (!defenderPresent) {
             return
@@ -134,18 +122,19 @@ $(document).ready(function () {
         healthPoints[defenderIndex] = healthPoints[defenderIndex] - attack[attackerIndex];
         $(".health" + attackerIndex).html(healthPoints[attackerIndex]);
         $(".health" + defenderIndex).html(healthPoints[defenderIndex]);
-        attack[attackerIndex] = (attack[attackerIndex] + baseAttack[attackerIndex]);
+        
         $("#theAttack").html("You Attacked " + name[defenderIndex] + " for " + attack[attackerIndex] + " Damage" )
         $("#theCounterAttack").html(name[defenderIndex] + " Attacked You for " + counterAttack[defenderIndex] + " Damage" )
-        
-        
+        attack[attackerIndex] = (attack[attackerIndex] + baseAttack[attackerIndex]);
+        // Attacker Defeats Defender
         if (healthPoints[defenderIndex] < 1) {
             $("#" + (defenderIndex + 30)).empty();
-            $("#theAttack").html("You Defeated" + name[defenderIndex]);
+            $("#theAttack").html("You Defeated " + name[defenderIndex]);
             $("#theCounterAttack").html("choose another enemy to attack");
             defenderPresent = false;
             wins++;
         }
+        // Defender Defeats Attacker
         if (healthPoints[attackerIndex] < 1) {
             alert("you have lost")
             $(".attack").hide();
@@ -154,7 +143,7 @@ $(document).ready(function () {
             $("#theAttack").html("You have been defeated by " + name[defenderIndex]);
             $("#theCounterAttack").html("Click restart to play again");
         }
-        
+        // Attacker wins game by defeating all Enemies
         else if (wins === 3) {
             alert("you have won!")
             $(".attack").hide();
@@ -163,13 +152,18 @@ $(document).ready(function () {
             $("#theAttack").html("You have defeated all dirtbags!");
             $("#theCounterAttack").html("Click restart to play again");
         }
-
-
-
-
-
     });
 
-
-
+    
+    // Restart Game
+    $(".restart").on("click", function () {
+        $(".enemy, .defender, .your-character").empty();
+        healthPoints = [120, 100, 150, 180];
+        attack = [8, 10, 6, 4];
+        defenderPresent = false;
+        wins = 0;
+        $(".restart").hide();
+        $(".attack").show();
+        startGame(); 
+});
 });
